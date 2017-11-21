df55eadbb022c67dafde9f73f6470a2d47c0b1a8
Lpan <span4718@163.com>
2017-11-21 17:49:14 +0800

    password and username

---
+++ src/main/java/com/Lpan/test/ramType/job/MyTestBean.java
@@ -0,0 +1,10 @@
+package com.Lpan.test.ramType.job;
+
+import org.springframework.beans.factory.InitializingBean;
+
+public class MyTestBean implements InitializingBean {
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+	}
+}
